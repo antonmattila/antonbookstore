2a63baace1a39f48a2adee918616ae5f33b2884a
bhi060@myy.haaga-helia.fi <bhi060@myy.haaga-helia.fi>
2024-09-17 08:42:47 +0300

    first commit

---
+++ src/main/java/antonbookstore/antonbookstore/AntonbookstoreApplication.java
@@ -0,0 +1,13 @@
+package antonbookstore.antonbookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntonbookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntonbookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/antonbookstore/antonbookstore/AntonbookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package antonbookstore.antonbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntonbookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
